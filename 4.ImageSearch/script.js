46700917a129d26aab12985fb97c3c3aa00396bf
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-17 12:45:16 +0530

    add new project

--- 4.ImageSearch/script.js
+++ 4.ImageSearch/script.js
@@ -2,9 +2,29 @@ document.addEventListener('DOMContentLoaded', function () {
     const forms = document.querySelector('form');
     const photo = document.querySelector('.images');
     const searchInput = document.querySelector('.search');
+    const key = 'pxzg1CziUJ63ABH6htWqFelJwSJzoiWOpqzHaIIjgYvgMsikGIyB3oJ2'
 
     forms.addEventListener('submit', function (e) {
         e.preventDefault();
-        console.log('hello');
+        const search = searchInput.value.trim();
+        searchPhotos(search)
+        console.log(search);
     });
+
+    async function searchPhotos(term) {
+        photo.innerHTML = ""
+
+        const link = `https://api.pexels.com/v1/search?query=${term}&per_page=9`
+
+        const response = await fetch(link, {
+            method: 'GET',
+            headers: {
+                Accept: "application/json",
+                Authorization: key
+            }
+        })
+        const data=await response.json();
+        
+        console.log(data);
+    }
 });
