940daa23c704e1d82d87a88777a9e652d5dd7f25
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-18 18:48:01 +0530

    complete login

--- 5.PasswordGEnerator/script.js
+++ 5.PasswordGEnerator/script.js
@@ -1,3 +1,10 @@
 function generate(length=12){
-    console.log("password")
+    let char="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890?><:}{+_,./;'[]'}!@#$%^&*"
+
+    let password=""
+    for(var i=0;i<length;i++){
+        var c = Math.floor(Math.random() * char.length);
+        password +=char[c]
+    }
+    document.querySelector(".password").textContent=password;
 }
