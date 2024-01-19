4dea4c666676caf1fb576b45c540f06cdff74399
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-19 21:26:20 +0530

    logic part complete

--- 6.QrGenerator/script.js
+++ 6.QrGenerator/script.js
@@ -1,5 +1,25 @@
-function QrCode(){
-    let inpt=document.querySelector('input').value;
+function QrCode() {
+    let inpt = document.querySelector('input').value;
+    let img = document.querySelector('.img')
+
+    fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inpt}`)
+        .then(response => response.blob())
+        .then(blob => {
+            const img = document.createElement("img");
+            const url = URL.createObjectURL(blob);
+            img.src = url;
+            img.height = '200';
+            img.onload = () => {
+                const loader = document.getElementById("loader");
+                if (loader) {
+                    loader.remove();
+                }
+            };
+            img.alt = "QR Code";
+            img.classList.add('show');
+            document.body.appendChild(img);
+        })
+
 
 
 }
