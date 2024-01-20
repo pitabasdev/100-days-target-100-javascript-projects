58d1282cf7863aba96448a6c8e946f8226356f76
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-20 17:20:37 +0530

    complete logic

--- 7.QuoteGenerator/script.js
+++ 7.QuoteGenerator/script.js
@@ -1,8 +1,21 @@
-function Quote() {
-    let num = 0;
-    let quote = []
-    fetch("https://type.fit/api/quotes")
-        .then(function (response) {
-            return response.json();
-        })
-}
+let btn = document.querySelector('button')
+let num = 0;
+let quote = [];
+let h = document.querySelector('h1');
+
+fetch("https://type.fit/api/quotes")
+    .then(function (response) {
+        return response.json();
+    })
+    .then((data) => {
+        quote = data;
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+btn.addEventListener('click', () => {
+    num++;
+    h.innerText = quote[num].text;
+
+})
