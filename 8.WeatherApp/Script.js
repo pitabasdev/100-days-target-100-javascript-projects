12ffededc9e1efab113dae462fadd6cf7e9ca385
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-22 23:14:43 +0530

    successfully fetch location key

--- 8.WeatherApp/Script.js
+++ 8.WeatherApp/Script.js
@@ -2,6 +2,10 @@ function weather() {
     const key = '8ZE3wl5cMpyuWL5CGqGDU5kAVOouGcUD'
     let city = document.getElementById('city').value;
     fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${city}`)
-    console.log(city)
-    
+        .then(response => response.json())
+        .then((data) => {
+
+            let locationKey = data[0].Key
+            console.log()
+        })
 }
