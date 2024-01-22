dddc826a18b3c570c1172faa4d79ee308a006604
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-22 23:09:00 +0530

    write some logic

--- 8.WeatherApp/Script.js
+++ 8.WeatherApp/Script.js
@@ -1,3 +1,7 @@
-function weather(){
+function weather() {
+    const key = '8ZE3wl5cMpyuWL5CGqGDU5kAVOouGcUD'
+    let city = document.getElementById('city').value;
+    fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${city}`)
+    console.log(city)
     
 }
