f0f9f3298a398940defb1591d3942d21c44edd41
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-24 13:48:33 +0530

    start js logic

---
+++ 9.BackgroundGenerator/script.js
@@ -0,0 +1,4 @@
+let color1=document.querySelector('.color1');
+let color2=document.querySelector('.color2')
+let h3=document.getElementsByTagName('h3')
+let bgcolor=document.querySelector('.gradeint')
