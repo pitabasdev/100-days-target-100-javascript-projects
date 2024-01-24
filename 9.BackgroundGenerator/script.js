1d2fabf5eb974b778fcc3bed28e1413c9d976880
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-24 13:51:03 +0530

    javascript logic complete

--- 9.BackgroundGenerator/script.js
+++ 9.BackgroundGenerator/script.js
@@ -1,4 +1,17 @@
-let color1=document.querySelector('.color1');
-let color2=document.querySelector('.color2')
-let h3=document.getElementsByTagName('h3')
-let bgcolor=document.querySelector('.gradeint')
+let color1 = document.querySelector('.color1');
+let color2 = document.querySelector('.color2')
+let h3 = document.getElementsByTagName('h3')
+let body = document.querySelector('.gradeint')
+
+function Color() {
+    body.style.background = "linear-gradient(to right, "
+        + color1.value
+        + ", "
+        + color2.value
+        + ")";
+
+    css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener('input', Color)
+color2.addEventListener('input', Color)
