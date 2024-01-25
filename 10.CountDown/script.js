67212f2ab51e6383ddcb7da24a926b0c7eab827a
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-25 21:28:14 +0530

    javascript logic complete

--- 10.CountDown/script.js
+++ 10.CountDown/script.js
@@ -2,5 +2,17 @@ function timeCount(){
     let div=document.querySelector('.time')
     let input=document.querySelector('.addTime').value
 
-    console.log(input)
+    let interval=setInterval(()=>{
+        const hour=Math.floor(input/3600);
+        const minutes=Math.floor((input % 3600)/60)
+        const second=input%60;
+        div.textContent=`${String(hour).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(second).padStart(2, '0')}`;
+
+        if(input===0){
+            clearInterval(interval);
+            alert('Timer is finished!');
+        }else{
+            input--;
+        }
+    },1000)
 }
