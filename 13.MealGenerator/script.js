e3c7c0dd147620ca8248815b938bab677f686494
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-28 21:35:19 +0530

    complete js logic

--- 13.MealGenerator/script.js
+++ 13.MealGenerator/script.js
@@ -1,9 +1,23 @@
 function Generate() {
     var meal = document.querySelector('.meal-container')
     meal.innerHTML = ''
-    fetch('www.themealdb.com/api/json/v1/1/random.php')
-        .then(response => response.json())
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        .then(response =>
+            response.json()
+        )
         .then(data => {
-            console.log(data)
+            const mealdiv = document.createElement('div');
+            mealdiv.classList.add('meal');
+            mealdiv.innerHTML = `
+            <h3>${data.meals[0].strMeal}</h3>
+            <img src="${data.meals[0].strMealThumb}">
+            <p>${data.meals[0].strInstructions}</p>
+            `;
+
+            meal.appendChild(mealdiv);
+            console.log(data.meals[0]);
         })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 }
