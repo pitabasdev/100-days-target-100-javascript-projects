acc085253a59f072a7d2dbe7de447773c2a3c92c
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-28 21:01:54 +0530

    add meal api

--- 13.MealGenerator/script.js
+++ 13.MealGenerator/script.js
@@ -1,4 +1,9 @@
-function Generate(){
-    var meal=document.querySelector('.meal-container')
-    meal.innerHTML=''
+function Generate() {
+    var meal = document.querySelector('.meal-container')
+    meal.innerHTML = ''
+    fetch('www.themealdb.com/api/json/v1/1/random.php')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
 }
