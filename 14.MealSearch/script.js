1bc3b2c74dbd74768ba326fe916bd28bab14322b
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-29 14:50:08 +0530

    start js logic

--- 14.MealSearch/script.js
+++ 14.MealSearch/script.js
@@ -1,10 +1,12 @@
 function search() {
     var input = document.querySelector('.input-data').value;
 
-    fetch(`www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
+
         .then(response => response.json())
-        .then((date) => {
-            console.log(date);
+        .then((data) => {
+
+            console.log(data.meals);
         })
         .catch((err) => {
             alert("Error");
