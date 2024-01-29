eb652b47b5049fc9ae7ae04c91047c5f88274317
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-29 14:46:24 +0530

    add meals api

--- 14.MealSearch/script.js
+++ 14.MealSearch/script.js
@@ -1,4 +1,13 @@
-function search(){
-    var input=document.querySelector('.input-data')
+function search() {
+    var input = document.querySelector('.input-data').value;
+
+    fetch(`www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
+        .then(response => response.json())
+        .then((date) => {
+            console.log(date);
+        })
+        .catch((err) => {
+            alert("Error");
+        })
 
 }
