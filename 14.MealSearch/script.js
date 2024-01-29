43b2bf5dbcf1fdf9ada035a49c0f23b9539157fd
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-29 15:27:22 +0530

    complete js logic

--- 14.MealSearch/script.js
+++ 14.MealSearch/script.js
@@ -1,5 +1,7 @@
 function search() {
     var input = document.querySelector('.input-data').value;
+    var div = document.querySelector('.data')
+    div.innerHTML = ''
 
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
 
@@ -7,6 +9,13 @@ function search() {
         .then((data) => {
             data.meals.forEach((meal) => {
                 console.log(meal)
+                let meald = document.createElement('div')
+                meald.innerHTML = `<h2>${meal.strMeal}</h2>
+                <img src="${meal.strMealThumb}">
+                <p>${meal.strInstructions}</p>
+                `;
+                div.appendChild(meald)
+
             })
 
         })
