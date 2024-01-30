af58da270f0000548d62fe31d71270312071846b
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-30 18:54:42 +0530

    complete js logic

--- 15.AgeCalculator/script.js
+++ 15.AgeCalculator/script.js
@@ -1,8 +1,9 @@
 function Calculate() {
     let birthday = document.getElementById('birthday').value;
     var today = new Date();
-    var birth = new Date(birthday)
-    var age = today.getFullYear - birth.getFullYear;
+    var birth = new Date(birthday);
+    var age = today.getFullYear() - birth.getFullYear();
+    const result = document.querySelector('.result').innerHTML = age;
 
-    console.log(age)
+    console.log(age);
 }
