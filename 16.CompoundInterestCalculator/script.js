8f82f8f187cff1a4682e56c21e3a5b7041b6aead
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-31 21:34:20 +0530

    complete js logic

--- 16.CompoundInterestCalculator/script.js
+++ 16.CompoundInterestCalculator/script.js
@@ -5,11 +5,16 @@ function Calculate() {
     let years = document.getElementById('years').value;
     let NumTime = document.getElementById('nomtime').value;
     console.log(amount, Interest, years, NumTime)
+    let result=document.querySelector('.result')
 
     if(amount>0 && Interest>0 && NumTime>0 && years>0){
         //Calculating the Compound interest
         CI=(amount*Math.pow((1+(Interest/(NumTime*100))),(NumTime*years)));
         CI=CI.toFixed(2)
+        //Displaying the results in HTML
+        result.innerHTML=CI
+    }else{
+        alert("Please enter valid inputs")
     }
 
 }
