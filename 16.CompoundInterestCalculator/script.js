383d4e718d751c05f432b4115d38f889cb6386b7
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-31 21:27:46 +0530

    inside js select add html

---
+++ 16.CompoundInterestCalculator/script.js
@@ -0,0 +1,8 @@
+function Calculate() {
+    let amount = document.querySelector('.form-amount').value;
+    let Interest = document.querySelector('.form-interest').value;
+    let years = document.getElementById('years').value;
+    let NumTime = document.getElementById('nomtime').value;
+    console.log(amount, Interest, years, NumTime)
+
+}
