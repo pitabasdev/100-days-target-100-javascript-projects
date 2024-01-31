0ddccb2e2a483ce1e25b9183c8d33a2de79d5524
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-01-31 21:32:16 +0530

    add one div for result displaying

--- 16.CompoundInterestCalculator/script.js
+++ 16.CompoundInterestCalculator/script.js
@@ -1,8 +1,15 @@
 function Calculate() {
+    var CI = 0;
     let amount = document.querySelector('.form-amount').value;
     let Interest = document.querySelector('.form-interest').value;
     let years = document.getElementById('years').value;
     let NumTime = document.getElementById('nomtime').value;
     console.log(amount, Interest, years, NumTime)
+
+    if(amount>0 && Interest>0 && NumTime>0 && years>0){
+        //Calculating the Compound interest
+        CI=(amount*Math.pow((1+(Interest/(NumTime*100))),(NumTime*years)));
+        CI=CI.toFixed(2)
+    }
 
 }
