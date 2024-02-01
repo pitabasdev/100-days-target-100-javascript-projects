c5bca2defbb2c9b268115744cdf9710b0f09036f
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-02-01 18:55:20 +0530

    update js logic

--- 17.JokesTeller/script.js
+++ 17.JokesTeller/script.js
@@ -1,10 +1,18 @@
-function Jokes(){
+function Jokes() {
     fetch('https://v2.jokeapi.dev/joke/Any')
-    .then(response=>response.json())
-    .then((data)=>{
-        console.log(data)
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
+        .then(response => response.json())
+        .then((data) => {
+            if (data.type == "twopart") {
+                document.querySelector(".jokes").innerHTML = `<strong>${data.setup}</strong><br> ${data.delivery}`;
+            } else {
+                document.querySelector(".jokes").innerHTML = `<strong>${data.joke}</strong>`;
+            }
+
+
+
+
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 }
