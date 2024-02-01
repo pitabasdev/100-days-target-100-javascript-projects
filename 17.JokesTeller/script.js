d64bcd83004a17bdba81ad152deb87fdea50be84
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-02-01 16:48:23 +0530

    using then method handle

--- 17.JokesTeller/script.js
+++ 17.JokesTeller/script.js
@@ -1,4 +1,10 @@
 function Jokes(){
     fetch('https://v2.jokeapi.dev/joke/Any')
     .then(response=>response.json())
+    .then((data)=>{
+        console.log(data)
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
 }
