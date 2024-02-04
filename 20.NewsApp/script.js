fcbc5b8a3c48e9e3c962d6034bded6db1913b67e
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-02-04 10:07:10 +0530

    add news api

--- 20.NewsApp/script.js
+++ 20.NewsApp/script.js
@@ -1,3 +1,11 @@
-function News(){
-    const api="50b26d6ff2284aaab2978fce0bf1b7f2"
+function News() {
+    const api = "50b26d6ff2284aaab2978fce0bf1b7f2"
+    fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${api}`)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data)
+        })
+        .catch((err) => {
+            console.error(err);
+        })
 }
