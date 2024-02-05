41407ee343f4e975cc97aaf530980b513563c7e0
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-02-05 23:05:55 +0530

    inside function just call the update function

--- 21.Counter/script.js
+++ 21.Counter/script.js
@@ -2,9 +2,11 @@ let num = document.getElementById('num')
 let val = 0;
 function Increment() {
     val++
+    Update()
 }
 function Decrement() {
     val--
+    Update()
 }
 function Update() {
     num.innerHTML = val
