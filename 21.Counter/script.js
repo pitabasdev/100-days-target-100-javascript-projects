d9c6c5f332f44d34b8aeb36d2a948401c4c27b13
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2024-02-05 23:03:55 +0530

    declear two functions

--- 21.Counter/script.js
+++ 21.Counter/script.js
@@ -1,2 +1,8 @@
-let num=document.getElementById('num')
-let val=0;
+let num = document.getElementById('num')
+let val = 0;
+function Increment() {
+    val++
+}
+function Decrement() {
+    val--
+}
